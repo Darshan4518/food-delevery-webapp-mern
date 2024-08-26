8437a635f71adb1603bd1894074d1b94727b3c21
Darshan S <hitmandarshan34@gmail.com>
2024-08-26 21:28:32 +0530

    redis  import changes

--- backend/config/resdis.config.js
+++ backend/config/resdis.config.js
@@ -1,8 +1,6 @@
 const redis = require("redis");
 
-const createClient = require("redis");
-
-const redisClient = createClient({
+const redisClient = redis.createClient({
   password: "pbNM472fAPdVnf6GcgA3HQN4W9XEPWjR",
   socket: {
     host: "redis-16313.c305.ap-south-1-1.ec2.redns.redis-cloud.com",
@@ -14,6 +12,13 @@ redisClient.on("error", (err) => {
   console.error("Redis error:", err);
 });
 
-redisClient.connect();
+redisClient
+  .connect()
+  .then(() => {
+    console.log("Connected to Redis successfully!");
+  })
+  .catch((err) => {
+    console.error("Redis connection error:", err);
+  });
 
 module.exports = redisClient;
